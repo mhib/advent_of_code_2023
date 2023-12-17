63a18903b66ab1b517d4a5d6de99d47e26270c5d
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2023-12-17 23:26:35 +0100

    It seems like bfs solution is faster than dijkstra so also including that

--- 17/17.rb
+++ 17/17.rb
@@ -161,6 +161,77 @@ def find_shortest_second(map)
 
 end
 
+def find_shortest_second_bfs(map)
+  q = []
+
+  length = map.size
+  width = map[0].size
+
+  dist = Dist.new(length, width, 11)
+
+  is_pos_valid = lambda do |pos|
+    pos[0] >= 0 && pos[0] < length && pos[1] >= 0 && pos[1] < width
+  end
+
+  destination =  [length - 1, width - 1]
+
+  right = State.new([0, 1], RIGHT, 1)
+  down = State.new([1, 0], DOWN, 1)
+
+  dist[right] = map[0][1]
+  dist[down] = map[1][0]
+
+
+  q.push([right, map[0][1]])
+  q.push([down, map[1][0]])
+
+  while q.any?
+    state, d = q.shift
+
+    next if d != dist[state]
+
+    if state.count >= 4
+      OPPOSITE_MAPPINGS[state.direction].each do |dir|
+        new_pos = apply_move(state.pos, dir)
+        next unless is_pos_valid[new_pos]
+        new_state = State.new(new_pos, dir, 1)
+
+        prev_d = dist[new_state]
+        new_d = d + map[new_pos[0]][new_pos[1]]
+        if prev_d.nil? || prev_d > new_d
+          q.push([new_state, new_d])
+          dist[new_state] = new_d
+        end
+      end
+    end
+
+    if state.count < 10
+      new_pos = apply_move(state.pos, state.direction)
+      next unless is_pos_valid[new_pos]
+      new_state = State.new(new_pos, state.direction, state.count + 1)
+      prev_d = dist[new_state]
+      new_d = d + map[new_pos[0]][new_pos[1]]
+      if prev_d.nil? || prev_d > new_d
+        q.push([new_state, new_d])
+        dist[new_state] = new_d
+      end
+    end
+  end
+
+  min = 1.0 / 0
+
+  0.upto(3) do |direction|
+    4.upto(10) do |count|
+      if (val = dist[State.new(destination, direction, count)])
+        min = [val, min].min
+      end
+    end
+  end
+
+  min
+
+end
+
 @map = File.readlines('in.txt', chomp: true).map { _1.each_char.map(&:to_i) }
 
 p find_shortest_second(@map)
