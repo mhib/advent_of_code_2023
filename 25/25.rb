c1a0a92b770f27d595868f90d1a2a68e64080366
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2023-12-29 11:02:20 +0100

    Add slower Karger-Stein implementation

--- 25/25.rb
+++ 25/25.rb
@@ -47,6 +47,18 @@ class UnionFind
     end
     s.map { @sizes[_1] }
   end
+
+  def dup
+    dup = UnionFind.allocate
+    dup.count = count
+    dup.sizes = @sizes.dup
+    dup.sets = @sets.dup
+    dup
+  end
+
+  protected
+
+  attr_writer :count, :sizes, :sets
 end
 
 def karger(node_edges, edges)
@@ -77,4 +89,54 @@ def karger(node_edges, edges)
   end
 end
 
+def karger_stein_helper(node_edges, edges)
+  node_to_idx = node_edges.keys.map.with_index.to_h
+  edges = edges.map do |nodes|
+    nodes.map(&node_to_idx)
+  end
+  uf = UnionFind.new(node_to_idx.size)
+  while true
+    res = fast_min_cut(edges.dup, uf.dup)
+    return res if res
+  end
+end
+
+SQRT_2 = Math.sqrt(2)
+def fast_min_cut(i_edges, i_uf)
+  stack = [[i_edges, i_uf]]
+  while stack.any?
+    edges, uf = stack.pop
+    if uf.count <= 6
+      res_edges, res_uf = contract(edges, uf, 2)
+      if res_edges.size == 3
+        return uf.set_sizes.reduce(&:*)
+      end
+      next
+    end
+    t = (uf.count / SQRT_2).ceil + 1
+    stack << contract(edges, uf.dup, t)
+    stack << contract(edges, uf, t)
+  end
+  return nil
+end
+
+def contract(edges, uf, t)
+  i = 0
+  while uf.count > t
+    random_i = rand((i...edges.size))
+    edges[i], edges[random_i] = edges[random_i], edges[i]
+    l, r = edges[i]
+    i += 1
+    uf.union(l, r)
+  end
+
+  new_edges = edges[i..-1]
+  new_edges.reject! { |l, r| uf.find(l) == uf.find(r) }
+  [
+    new_edges,
+    uf
+  ]
+end
+
 p karger(node_edges, edges)
+# p karger_stein_helper(node_edges, edges)
